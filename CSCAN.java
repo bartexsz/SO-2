023dbb38696678f5d1aeba2af8142d626dace522
BillieJane <b.j.lewandowska@gmail.com>
2014-05-14 21:14:48 +0200

    Addes CSCAN class and changed scan class

--- CSCAN.java
+++ CSCAN.java
@@ -1,32 +1,18 @@
 import java.util.ArrayList;
 
-public class SCAN extends Algorithm {
+public class CSCAN extends Algorithm {
 
 	int position=1;
 	int preposition=0;
 	
-	public SCAN(ArrayList<Request> list){
+	public CSCAN(ArrayList<Request> list){
 		super(list);
 	}
 	
 	public Request activeRequest(){
 		if(position==200){
-			position=199;
-			preposition=200;
-			for(int i=0; i<list.size(); i++){
-				if(list.get(i).position==position) return list.get(i);
-			}
-			return null;
-		}else if(position==1 && preposition==2){
-			position++;
-			preposition--;
-			for(int i=0; i<list.size(); i++){
-				if(list.get(i).position==position) return list.get(i);
-			}
-			return null;
-		}else if(preposition>position){
-			position--;
-			preposition--;
+			position=1;
+			preposition=0;
 			for(int i=0; i<list.size(); i++){
 				if(list.get(i).position==position) return list.get(i);
 			}
