63a7928a561f83b65528b43af4cbbdc7dff017a6
Bartłomiej Szcześniak <bartexsz@gmail.com>
2014-05-14 04:30:33 +0200

    Added Disk class

---
+++ Disk.java
@@ -0,0 +1,11 @@
+public class Disk {
+	public static int pos;
+	public static int size;
+	public static int changes;
+	public static void init()
+	{
+		pos = 0;
+		size = 200;
+		changes = 0;
+	}
+}
